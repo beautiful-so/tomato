5e1f3941528d77b715d40dbcc5d663c4425310c6
chae <sungu.che@icloud.com>
2018-05-07 23:01:56 +0900

    11

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
-const {Video, ImageView, fs, ui} = require('tabris');
+const {ImageView, fs, ui} = require('tabris');
 
-let file = fs.cacheDir + '/12943877.mp4';
+let file = fs.cacheDir + '/test.png';
 
-let video = new Video({
-  left: 0, top: 0, right: 0, bottom: 0,controlsVisible: true
+let imageView = new ImageView({
+  centerX: 0, centerY: 0, width: 400, height: 200,
+  background: '#aaaaaa'
 }).appendTo(ui.contentView);
 
-fetch("https://gcs-vimeo.akamaized.net/exp=1525672660~acl=%2A%2F12943877.mp4%2A~hmac=8bb93f1cbf417a542e279980f57bdff0f63335c1fba48ad037c09cccaca89332/vimeo-prod-skyfire-std-us/01/216/0/1084537/12943877.mp4")
+fetch('http://211.245.90.248:8080/fff.png')
   .then(res => res.arrayBuffer())
   .then(data => fs.writeFile(file, data))
-  .then(() => video.url = file)
-  .then(() => {
-	console.log('image:', file );
-  })
+  .then(() => imageView.image = file)
+  .then(() => console.log('image:', file))
   .catch(err => console.error(err));
