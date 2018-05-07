900f50787ae34b14e1f48ba462e6878cbad1dd03
chae <sungu.che@icloud.com>
2018-05-07 18:54:19 +0900

    1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const {Video, ImageView, fs, ui} = require('tabris');
 
-let file = fs.filesDir + '/12943877.mp4';
+let file = fs.cacheDir + '/12943877.mp4';
 
 let video = new Video({
   left: 0, top: 0, right: 0, bottom: 0,controlsVisible: true
 }).appendTo(ui.contentView);
-video.url= "https://gcs-vimeo.akamaized.net/exp=1525672660~acl=%2A%2F12943877.mp4%2A~hmac=8bb93f1cbf417a542e279980f57bdff0f63335c1fba48ad037c09cccaca89332/vimeo-prod-skyfire-std-us/01/216/0/1084537/12943877.mp4";
+
 fetch("https://gcs-vimeo.akamaized.net/exp=1525672660~acl=%2A%2F12943877.mp4%2A~hmac=8bb93f1cbf417a542e279980f57bdff0f63335c1fba48ad037c09cccaca89332/vimeo-prod-skyfire-std-us/01/216/0/1084537/12943877.mp4")
   .then(res => res.arrayBuffer())
   .then(data => fs.writeFile(file, data))
